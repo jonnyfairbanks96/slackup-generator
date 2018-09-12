f6e0e684d1ebfb9315489e38921bf1e3bb6067fa
jonnyfairbanks96 <fairbanks.jonathan96@gmail.com>
2018-09-11 21:19:20 -0600

    Adds webhook to automatically post to slack
    - Uses ruby basic libraries to 'curl' a webhook for your slack workspace

--- template.rb
+++ template.rb
@@ -1,33 +1,58 @@
 #!/usr/bin/env ruby
 
-def render
-  print "Ticket: "
-  ticket = gets.chomp
+require 'net/http'
+require 'uri'
+require 'json'
 
-  print "PR: "
-  pr = gets.chomp
+class SlackUp
+  def self.render
+    print "Ticket: "
+    @@ticket = gets.chomp
 
-  print "Deploy: "
-  deploy = gets.chomp
+    print "PR: "
+    @@pr = gets.chomp
 
-  print "Concerns seperate by periods: "
-  concerns = gets.chomp
+    print "Deploy: "
+    @@deploy = gets.chomp
 
-  puts ticket, pr, deploy, concerns
-  puts format(ticket, pr, deploy, concerns)
-end
+    print "Concerns seperate by periods: "
+    concerns = gets.chomp
+    @@concerns = concerns.split('.').map { |con| "\"#{con.strip}\"" }
+  end
 
-def format(ticket, pr, deploy, concerns)
+  def self.format(ticket, pr, deploy, concerns)
 "
+```
+Jonny's Slackup
 {
   \"ticket\": #{ticket},
   \"pr\": #{pr},
   \"deploy\": #{deploy},
   \"concerns\": [
-      #{concerns.gsub(".", "\",\n      ")}
+      #{concerns.join(",\n      ")}
   ]
 }
+```
 "
+  end
+
+  def self.post_to_slack
+    uri = URI.parse("Your Slack Web Hook")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request.body = JSON.dump({
+      "text" => "#{self.format(@@ticket, @@pr, @@deploy, @@concerns)}"
+    })
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+  end
 end
 
-render
+SlackUp.render
+SlackUp.post_to_slack
