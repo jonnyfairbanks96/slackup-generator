5d1c362b26e323f922586405b5d1fc388b4d22ca
Jonathan Fairbanks <fairbanks.jonathan96@gmail.com>
2019-02-07 08:39:54 -0700

    Vim feature
    - Make slackup in vim

--- template.rb
+++ template.rb
@@ -6,64 +6,39 @@ require 'json'
 
 class SlackUp
   def self.render
-    print "Ticket: "
-    @@ticket = gets.chomp
-
-    print "PR: "
-    @@pr = gets.chomp
-
-    print "Deploy: "
-    @@deploy = gets.chomp
-
-    print "Title: "
-    @@title = gets.chomp
-
-    print "Update: "
-    @@concerns = gets.chomp
-
-    @@slack_up = self.format(@@ticket, @@pr, @@title, @@deploy, @@concerns)
+    time = Time.now
+    system("cp", "slackup_template.txt", ".slackup_#{time}.txt")
+    system("nvim", ".slackup_#{time}.txt")
+    system("cat", ".slackup_#{time}.txt")
   end
 
-  def self.post_to_slack
-    print "#{@@slack_up}\nWould you like to post to slack? (y/n): "
-
-    response = gets.chomp
-
-    if response.downcase == "y"
-      uri = URI.parse("Your Slack Web Hook")
-      request = Net::HTTP::Post.new(uri)
-      request.content_type = "application/json"
-      request.body = JSON.dump({
-        "text" => "#{@@slack_up}"
-      })
-
-      req_options = {
-        use_ssl: uri.scheme == "https",
-      }
-
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
-      end
-    elsif response.downcase == "n"
-      puts "Aborted"
-    else
-      puts "Invalid entry aborted."
-    end
-  end
-
-  private
-
-  def self.format(ticket, pr, title, deploy, concerns)
-"
-Jonny's Slackup
-*Ticket*: #{ticket}
-*PR*:     #{pr}
-*Title*:  #{title}
-*Deploy*: #{deploy}
-*Update*: #{concerns}
-"
-  end
+  # def self.post_to_slack
+  #   print "#{@@slack_up}\nWould you like to post to slack? (y/n): "
+  #
+  #   response = gets.chomp
+  #
+  #   if response.downcase == "y"
+  #     uri = URI.parse("Your Slack Web Hook")
+  #     request = Net::HTTP::Post.new(uri)
+  #     request.content_type = "application/json"
+  #     request.body = JSON.dump({
+  #       "text" => "#{@@slack_up}"
+  #     })
+  #
+  #     req_options = {
+  #       use_ssl: uri.scheme == "https",
+  #     }
+  #
+  #     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  #       http.request(request)
+  #     end
+  #   elsif response.downcase == "n"
+  #     puts "Aborted"
+  #   else
+  #     puts "Invalid entry aborted."
+  #   end
+  # end
 end
 
 SlackUp.render
-SlackUp.post_to_slack
+# SlackUp.post_to_slack
